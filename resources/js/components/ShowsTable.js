13811a0647322b3822d265347518efc007dda96e
Luke Randolph <lukerandolph116@gmail.com>
2019-11-08 20:58:09 -0500

    Edit shows table

--- resources/js/components/ShowsTable.js
+++ resources/js/components/ShowsTable.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import shows from "../../../public/resources/shows.json";
-
+//hello
 const showsTableContainer = {
     backgroundColor: "rgba(0,0,0,.8)",
     border: "1px solid #dddddd",
@@ -35,33 +35,36 @@ export default class ShowsTable extends Component {
     }
 
     render() {
-			console.log(this.state.shows)
-        return (
-            <div
-                style={showsTableContainer}
-                className="row justify-content-center shows-table"
-            >
-                <div style={{ ...tableRow, ...tableRowHeader }}>
-                    <p style={tableItem}>VENUE</p>
-                    <p style={tableItem}>DATE</p>
-                    <p style={tableItem}>TIME</p>
-                    <p style={tableItem}>COVER</p>
-                </div>
-                {this.state.shows.map(show => {
-                    return (
-                        <div style={tableRow}>
-                            <p style={tableItem}>{show.name}</p>
-                            <p style={tableItem}>{show.date}</p>
-                            <p style={tableItem}>{show.time}</p>
-                            <p style={tableItem}>{show.price}</p>
-                        </div>
+        console.log(this.state.shows)
+        return ( <
+            div style = { showsTableContainer }
+            className = "row justify-content-center shows-table" >
+            <
+            div style = {
+                {...tableRow, ...tableRowHeader } } >
+            <
+            p style = { tableItem } > VENUE < /p> <
+            p style = { tableItem } > DATE < /p> <
+            p style = { tableItem } > TIME < /p> <
+            p style = { tableItem } > COVER < /p> <
+            /div> {
+                this.state.shows.map(show => {
+                    return ( <
+                        div style = { tableRow } >
+                        <
+                        p style = { tableItem } > { show.name } < /p> <
+                        p style = { tableItem } > { show.date } < /p> <
+                        p style = { tableItem } > { show.time } < /p> <
+                        p style = { tableItem } > { show.price } < /p> <
+                        /div>
                     );
-                })}
-            </div>
+                })
+            } <
+            /div>
         );
     }
 }
 
 if (document.getElementById("shows-table")) {
-    ReactDOM.render(<ShowsTable />, document.getElementById("shows-table"));
+    ReactDOM.render( < ShowsTable / > , document.getElementById("shows-table"));
 }
